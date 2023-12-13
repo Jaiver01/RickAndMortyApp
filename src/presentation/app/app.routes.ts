ae7a6f5285a09fc0c2d27a2f2c44fa94357e6f13
Jaiver Ariza <jaiver.ariza@doublevpartners.com>
2023-12-13 08:53:48 -0500

    Added routes to pages

--- src/presentation/app/app.routes.ts
+++ src/presentation/app/app.routes.ts
@@ -6,4 +6,39 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./pages/home/home.component').then((mod) => mod.HomeComponent),
   },
+  {
+    path: 'characters',
+    loadComponent: () =>
+      import('./pages/characters/characters.component').then(
+        (mod) => mod.CharactersComponent
+      ),
+  },
+  {
+    path: 'characters/:id',
+    loadComponent: () =>
+      import('./pages/characters/pages/character/character.component').then(
+        (mod) => mod.CharacterComponent
+      ),
+  },
+  {
+    path: 'episodes',
+    loadComponent: () =>
+      import('./pages/episodes/episodes.component').then(
+        (mod) => mod.EpisodesComponent
+      ),
+  },
+  {
+    path: 'locations',
+    loadComponent: () =>
+      import('./pages/locations/locations.component').then(
+        (mod) => mod.LocationsComponent
+      ),
+  },
+  {
+    path: 'locations/:id',
+    loadComponent: () =>
+      import('./pages/locations/pages/location/location.component').then(
+        (mod) => mod.LocationComponent
+      ),
+  },
 ];
