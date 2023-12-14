aa182e84f3c89277fdd3fa75740888c6d7054fd9
Jaiver Ariza <jaiver.ariza@doublevpartners.com>
2023-12-13 22:13:39 -0500

    Added search input

--- src/presentation/app/shared/components/header/header.component.ts
+++ src/presentation/app/shared/components/header/header.component.ts
@@ -1,7 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
 import { GetUserSettingsUseCase } from '../../../../../domain/usecases/get-user-settings.usecase';
 import { SetUserSettingsUseCase } from '../../../../../domain/usecases/set-user-settings.usecase';
+import {
+  selectCurrentPage,
+  selectFilters,
+} from '../../../core/store/selectors/filter.selectors';
+import { setFilters } from '../../../core/store/actions/filter.actions';
+import { CharacterFilter } from '../../../../../domain/models/character.model';
+import { EpisodeFilter } from '../../../../../domain/models/episode.model';
+import { LocationFilter } from '../../../../../domain/models/location.model';
 
 @Component({
   selector: 'app-header',
@@ -11,12 +21,41 @@ import { SetUserSettingsUseCase } from '../../../../../domain/usecases/set-user-
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('searchInput') searchInput?: ElementRef<HTMLInputElement>;
+
   isDarkTheme: boolean = true;
 
+  private timeout: any;
+  private currentPage: string = '';
+  private currentPageSubscription: Subscription;
+  private filters: {
+    characters?: CharacterFilter;
+    episodes?: EpisodeFilter;
+    locations?: LocationFilter;
+  } = {};
+  private filtersSubscription: Subscription;
+
   constructor(
+    private store: Store,
     private getUserSettingsUseCase: GetUserSettingsUseCase,
     private setUserSettingsUsecase: SetUserSettingsUseCase
-  ) {}
+  ) {
+    this.currentPageSubscription = this.store
+      .select(selectCurrentPage)
+      .subscribe({
+        next: (currentPage) => {
+          this.currentPage = currentPage;
+
+          if (this.searchInput) {
+            this.searchInput.nativeElement.value = '';
+          }
+        },
+      });
+
+    this.filtersSubscription = this.store.select(selectFilters).subscribe({
+      next: (filters) => (this.filters = filters),
+    });
+  }
 
   ngOnInit(): void {
     this.getUserSettingsUseCase.execute().subscribe({
@@ -26,10 +65,38 @@ export class HeaderComponent implements OnInit {
     });
   }
 
+  ngOnDestroy(): void {
+    this.currentPageSubscription.unsubscribe();
+    this.filtersSubscription.unsubscribe();
+  }
+
   toggleTheme(): void {
     this.isDarkTheme = !this.isDarkTheme;
     this.setUserSettingsUsecase.execute({
       theme: this.isDarkTheme ? 'dark' : 'retro',
     });
+  }
+
+  onSearch(): void {
+    if (!this.searchInput || this.currentPage === 'home') {
+      return;
+    }
+
+    const value = this.searchInput.nativeElement.value;
+
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      this.store.dispatch(
+        setFilters({
+          ...this.filters,
+          [this.currentPage]: {
+            ...(this.filters[
+              this.currentPage as 'characters' | 'episodes' | 'locations'
+            ] ?? {}),
+            name: value,
+          },
+        })
+      );
+    }, 400);
   }
 }
